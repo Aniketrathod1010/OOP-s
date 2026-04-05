d654efbfc9f173568c6c75f2b47aaca734418767
Aniketrathod1010 <udayrathod.cse24@sbjit.edu.in>
2026-04-05 10:06:24 +0530

    Add Student class with toString method

---
+++ 3.9.java
@@ -0,0 +1,19 @@
+class Student {
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString() {
+        return "ID: " + id + ", Name: " + name;
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student(101, "Aniket");
+
+        System.out.println(s1);
+    }
+}
