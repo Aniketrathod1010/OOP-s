e1d0ad40da2103833772c1ef787d7ab12aa96bb6
Aniketrathod1010 <udayrathod.cse24@sbjit.edu.in>
2026-04-05 10:01:42 +0530

    Create 3.1.java

---
+++ 3.1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+class UsernameValidator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = sc.nextLine();
+
+        username = username.trim().toLowerCase();
+
+        System.out.println("Cleaned Username: " + username);
+    }
+}
